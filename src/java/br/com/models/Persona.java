82e40424ca53f66cadcd54e761fa009f7e8b7080
Tayanne <tayh.snovaes@gmail.com>
2021-10-11 19:30:53 -0300

    add models

---
+++ src/java/br/com/models/Persona.java
@@ -0,0 +1,49 @@
+
+package br.com.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Tayanne
+ */
+public class Persona {
+    private int id;
+    private String nome;
+    private Date dataNascimento = new Date();
+    private String genero;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+}
